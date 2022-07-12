cf97a036eb0fbcdb9e42f73714f00ab335dc5add
carl10086 <yszlx200@163.com>
2022-07-12 12:21:21 +0800

    hanlp

---
+++ src/test/java/com/ysz/hanlp/learning/core/DefaultNatureParser.java
@@ -0,0 +1,16 @@
+package com.ysz.hanlp.learning.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DefaultNatureParser {
+
+
+  public static void main(String[] args) throws Exception {
+    String path = "/Users/carl/work/dt/tmp/nature.txt";
+    Map<String, String> tmp = new HashMap<>(256);
+
+
+  }
+
+}
